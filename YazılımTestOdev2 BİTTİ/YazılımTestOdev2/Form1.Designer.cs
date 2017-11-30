aa99910f10dd778f78f429a0cbcf1e0f87cb4660
nazlikrmn <karamannazli1997@hotmail.com>
2017-11-30 14:18:33 +0300

    son

---
+++ YazılımTestOdev2 BİTTİ/YazılımTestOdev2/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿using System;
+
+namespace YazılımTestOdev2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnBasla = new System.Windows.Forms.Button();
+            this.btnCiz = new System.Windows.Forms.Button();
+            this.pnlIcerik = new System.Windows.Forms.Panel();
+            this.btnFare = new System.Windows.Forms.Button();
+            this.btnPeynir = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnPeynir);
+            this.panel1.Controls.Add(this.btnFare);
+            this.panel1.Controls.Add(this.btnBasla);
+            this.panel1.Controls.Add(this.btnCiz);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(571, 0);
+            this.panel1.MaximumSize = new System.Drawing.Size(132, 0);
+            this.panel1.MinimumSize = new System.Drawing.Size(132, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(132, 511);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnBasla
+            // 
+            this.btnBasla.Location = new System.Drawing.Point(13, 67);
+            this.btnBasla.Name = "btnBasla";
+            this.btnBasla.Size = new System.Drawing.Size(106, 23);
+            this.btnBasla.TabIndex = 7;
+            this.btnBasla.Tag = "-1,-1";
+            this.btnBasla.Text = "Başla";
+            this.btnBasla.UseVisualStyleBackColor = true;
+            this.btnBasla.Click += new System.EventHandler(this.btnBasla_Click);
+            // 
+            // btnCiz
+            // 
+            this.btnCiz.Location = new System.Drawing.Point(13, 38);
+            this.btnCiz.Name = "btnCiz";
+            this.btnCiz.Size = new System.Drawing.Size(106, 23);
+            this.btnCiz.TabIndex = 6;
+            this.btnCiz.Tag = "-1,-1";
+            this.btnCiz.Text = "Çiz";
+            this.btnCiz.UseVisualStyleBackColor = true;
+            this.btnCiz.Click += new System.EventHandler(this.btnCiz_Click);
+            // 
+            // pnlIcerik
+            // 
+            this.pnlIcerik.AllowDrop = true;
+            this.pnlIcerik.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlIcerik.Location = new System.Drawing.Point(0, 0);
+            this.pnlIcerik.Name = "pnlIcerik";
+            this.pnlIcerik.Size = new System.Drawing.Size(571, 511);
+            this.pnlIcerik.TabIndex = 1;
+            // 
+            // btnFare
+            // 
+            this.btnFare.AllowDrop = true;
+            this.btnFare.BackgroundImage = global::YazılımTestOdev2.Resource1.fare;
+            this.btnFare.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnFare.Location = new System.Drawing.Point(0, 138);
+            this.btnFare.Name = "btnFare";
+            this.btnFare.Size = new System.Drawing.Size(48, 46);
+            this.btnFare.TabIndex = 8;
+            this.btnFare.UseVisualStyleBackColor = true;
+            this.btnFare.Click += new System.EventHandler(this.btnFare_Click);
+            // 
+            // btnPeynir
+            // 
+            this.btnPeynir.AllowDrop = true;
+            this.btnPeynir.BackgroundImage = global::YazılımTestOdev2.Resource1.peynir;
+            this.btnPeynir.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPeynir.Location = new System.Drawing.Point(0, 190);
+            this.btnPeynir.Name = "btnPeynir";
+            this.btnPeynir.Size = new System.Drawing.Size(48, 46);
+            this.btnPeynir.TabIndex = 9;
+            this.btnPeynir.UseVisualStyleBackColor = true;
+            this.btnPeynir.Click += new System.EventHandler(this.btnPeynir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 511);
+            this.Controls.Add(this.pnlIcerik);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+       
+
+       
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnBasla;
+        private System.Windows.Forms.Button btnCiz;
+        private System.Windows.Forms.Panel pnlIcerik;
+        private System.Windows.Forms.Button btnPeynir;
+        private System.Windows.Forms.Button btnFare;
+    }
+}
+
---
+++ YazılımTestOdev2 BİTTİ/YazılımTestOdev2/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YazılımTestOdev2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+        Button b;
+        int sayi;
+        public Image image;
+        int sayac1 = 0;
+        int sayac2 = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        bool[,] dizi;
+        Button[,] butonDizisi;
+        int sayi1Max, sayi2Max;
+        private void btnCiz_Click(object sender, EventArgs e)
+        {
+            int sayi1=10, sayi2=10;
+            dizi = new bool[sayi1, sayi2];
+            butonDizisi = new Button[sayi1, sayi2];
+            int i,j;
+            for(i=0;i<sayi1;i++)
+            {
+                for(j=0;j<sayi2;j++)
+                {
+                    dizi[i, j] = true ;
+                }
+            }
+            sayi1Max = sayi1;
+            sayi2Max = sayi2;
+            Ciz(sayi1, sayi2);
+        }
+        private void Ciz(int Sayi1, int Sayi2)
+        {
+            int i, j;
+            for (i = 0; i < Sayi1; i++)
+            {
+                for (j = 0; j < Sayi2; j++)
+                {
+                    if (i == 0)
+                        dizi[i, j] = false;
+                    else if (j == 0)
+                        dizi[i, j] = false;
+                    else if (j == Sayi2 - 1)
+                        dizi[i, j] = false;
+                    else if (i == Sayi1 - 1)
+                        dizi[i, j] = false;
+                }
+            }
+            int Point1 = 0, Point2 = 0;
+            for ( i = 0; i < Sayi1; i++)
+            {
+                for (j = 0; j < Sayi2; j++)
+                {
+                    b = new Button();
+                    
+                  
+                    if (dizi[i, j] == false)
+                    {
+                        b.Image = Resource1.duvar;
+                    }
+                    else
+                    {
+                        b.Text = "";
+                        b.BackColor = Color.Transparent;
+                    }
+
+                    b.FlatStyle = FlatStyle.Flat;
+                    b.Top = Point2;
+                    b.Left = Point1;
+                    b.Height = 50;
+                    b.Width = 50;
+                    b.Tag = i + "," + j;
+             
+                    // b.Click += btnDuvar_Click;
+
+                    b.Click += new EventHandler(dinamikMetod);
+                    /*if (image == Resource1.fare)
+                        b.Click += btnFare_Click;
+                    else if (image == Resource1.peynir)
+                        b.Click += btnPeynir_Click;
+                    else*/
+                   
+                    pnlIcerik.Controls.Add(b);
+                    butonDizisi[i, j] = b;
+                    if (j == Sayi2 - 1)
+                    {
+                        Point1 = 0;
+                        Point2 += 50;
+                    }
+                    else
+                    {
+                        Point1 += 50;
+                    }
+                }
+            }
+
+
+                /* b.Click += new EventHandler(dinamikMetod);
+                   if (image == Resource1.fare)
+                       b.Click += btnFare_Click;
+                   else if (image == Resource1.peynir)
+                       b.Click += btnPeynir_Click;
+                   else
+                       b.Click += btnDuvar_Click;
+                   pnlIcerik.Controls.Add(b);*/
+        }
+        public Point startLocation;
+        public Point endLocation;
+        public void dinamikMetod(object sender,EventArgs e)
+        {
+            Button buton = (sender as Button);
+
+            /*  if (image.Flags == Resource1.fare.Flags)
+                 buton.Image=image;
+            else if (image.Flags == Resource1.peynir.Flags)
+                 buton.Image = image;
+              else
+                  buton.Image = Resource1.duvar;*/
+            Point tıklanan=new Point();
+            for (int i = 0; i < sayi1Max; i++)
+            {
+                for (int j = 0; j < sayi2Max; j++)
+                {
+                    if (butonDizisi[i, j] == buton)
+                    {
+                        tıklanan.X = i;
+                        tıklanan.Y = j;
+                    }
+                }
+            }
+            if (sayi==2)
+            {
+                buton.Image = image;
+                startLocation = tıklanan;
+            }
+           else if (sayi==1)
+            {
+                buton.Image = image;
+                endLocation = tıklanan;
+            }
+            else
+            {
+                buton.Image = Resource1.duvar;
+                dizi[tıklanan.X, tıklanan.Y] = false;
+            }
+
+            sayi = 0;
+
+        }
+       /* private void btnDuvar_Click(object sender, EventArgs e)
+        {
+            Button b = sender as Button;
+            string[] bol = new string[2];
+            bol = b.Tag.ToString().Split(',');
+            b.Image = Resource1.duvar;
+            b.Text = "";
+            dizi[Convert.ToInt32(bol[0]), Convert.ToInt32(bol[1])] = -1;
+        }*/
+
+        private void btnFare_Click(object sender, EventArgs e)
+        {
+            
+           
+            if (sayac1 == 0)
+            {
+                //Button b = sender as Button;
+                //string[] bol = new string[2];
+                image = Resource1.fare;
+                sayi = 2;
+                //b.Click += new EventHandler(dinamikMetod);
+                b.Text = "";
+                sayac1++;
+            }
+            else
+                MessageBox.Show("1'den fazla fare yerleştirilemez");
+        }
+
+        private void btnPeynir_Click(object sender, EventArgs e)
+        {
+            
+           
+            if (sayac2 == 0)
+            {
+               /* Button b = sender as Button;
+                string[] bol = new string[2];*/
+                image = Resource1.peynir;
+                sayi = 1;
+                b.Text = "";
+                sayac2++;
+            }
+            else
+                MessageBox.Show("1'den fazla peynir yerleştirilemez");
+        }
+        private SearchParameters searchParameters;
+        private void btnBasla_Click(object sender, EventArgs e)
+        {
+            this.searchParameters = new SearchParameters(startLocation, endLocation, dizi);
+            pathFinder PathFinder = new pathFinder(searchParameters);
+            List<Point> path = PathFinder.FindPath();
+
+            btnCiz.Enabled = false;
+            btnBasla.Enabled = false;
+            btnFare.Enabled = false;
+            btnPeynir.Enabled = false;
+            Hareket(path);
+                       
+        }
+        private void Hareket(List<Point> yol)
+        {
+            foreach (Point point in yol)
+            {
+                butonDizisi[point.X, point.Y].Image = Resource1.images;
+                if (point == endLocation)
+                    MessageBox.Show("Mission Completed");
+            }
+        }
+        
+        
+    }
+}
